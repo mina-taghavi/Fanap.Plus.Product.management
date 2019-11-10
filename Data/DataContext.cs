2a487a80c7f6e8b2cbce1e82ac20dc30b738cd5d
mina.t <mina.t@MINAT-PC>
2019-11-10 12:42:53 +0330

    add project

---
+++ Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Fanap.Plus.Product_Management.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fanap.Plus.Product_Management.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+          : base(options)
+        {
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fanap.Plus.Product_Management.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreationDate { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "نام مدیر پروژه")]
+        public string ProjectManagementName { get; set; }
+        public string ProductOwnerName { get; set; }
+
+       // public string[] DevelopmentTeam { get; set; }
+        //public string[] OtherMembers { get; set; }
+    }
+}
